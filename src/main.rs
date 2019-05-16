02bd0944a84dc2f96bb5e3ca27659a9765a07866
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-05-16 16:05:49 +0200

    working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
+extern crate chrono;
 extern crate term;
 extern crate toml;
-extern crate chrono;
 
-#[macro_use]
 extern crate gumdrop;
 
 use gumdrop::Options;
 
 use std::env;
-use std::path::{Path, PathBuf};
-use std::path;
 use std::fs;
 use std::io::Read;
+use std::path::PathBuf;
 
-use core::fmt::Debug;
 use chrono::Datelike;
-use std::ops::Add;
+
+#[macro_use]
+extern crate serde;
 
 const CARGO_FILE: &str = "Cargo.toml";
+
+#[derive(Debug, Deserialize)]
+struct ManifestInfo {
+    package: PackageInfo,
+}
+
+#[derive(Debug, Deserialize)]
+struct PackageInfo {
+    name: String,
+    version: String,
+    authors: Vec<String>,
+    description: Option<String>,
+    repository: Option<String>,
+    keywords: Option<Vec<String>>,
+}
 
 #[derive(Debug, Options)]
 struct CitationOption {
@@ -33,58 +47,55 @@ struct CitationOption {
     overwrite: bool,
 }
 
+impl PackageInfo {
+    pub fn build_bibtex(&self) -> String {
+        let mut buf = String::from("@misc{");
+        buf = buf + self.name.as_str() + ",\n";
+        buf = buf + "\ttitle={{" + self.name.as_str();
+        buf = match &self.description {
+            None => buf,
+            Some(s) => buf + ": " + s.as_str(),
+        } + "}},\n";
+        buf = buf + "\tauthor={{" + self.authors.join(" and ").as_str() + "}},\n";
+        let t = chrono::prelude::Local::now();
+        let month = t.month();
+        let year = t.year();
+
+        let month_fmt = format!("\tmonth = {:0},\n", month);
+        let year_fmt = format!("\tyear = {:0},\n", year);
+        buf = buf + month_fmt.as_str() + year_fmt.as_str();
+        buf = match &self.repository {
+            None => buf,
+            Some(url) => buf + "\turl = {{" + url.as_str() + "}}\n",
+        };
+        buf = match &self.keywords {
+            None => buf,
+            Some(keywords) => buf + "\tkeywords = {" + keywords.join(", ").as_str() + "}\n",
+        };
+        buf + "}\n"
+    }
+}
+
 fn main() {
     let opt = CitationOption::parse_args_default_or_exit();
     let dir = env::current_dir().unwrap();
     let cargo_path = PathBuf::from(CARGO_FILE);
     let abs_path = dir.join(cargo_path);
 
-    println!("Hello, world!\n{:?}", opt);
-    println!("Hello, world!\n{:?}", abs_path);
     if !abs_path.exists() {
         println!("Current directory is not a Cargo project.");
         return;
     }
     let mut file = match fs::File::open(&abs_path) {
         Err(_) => panic!("Unable to read Cargo file from {:?}", &abs_path),
-        Ok(file) => file
+        Ok(file) => file,
     };
     let mut cargo_content = String::new();
-    file.read_to_string(&mut cargo_content);
-    println!("cargo content: {:}", cargo_content);
-    let r = build_bibtex("cargocite", vec![String::from("Mathieu Besançon")], "v0.1.0", "https", "description");
+    if let Err(e) = file.read_to_string(&mut cargo_content) {
+        panic!(e);
+    }
+    let t: ManifestInfo = toml::from_str(cargo_content.as_str()).unwrap();
+    println!("t: {:?}", t);
+    let r = t.package.build_bibtex();
     println!("{}", r);
-}
-
-fn build_bibtex(name: &str, authors: Vec<String>, version: &str, url: &str, description: &str) -> String {
-    let mut buf = String::from("@misc{");
-    buf = buf
-        .add(name)
-        .add(",\n");
-    buf = buf
-        .add("\ttitle={{")
-        .add(name)
-        .add(": ")
-        .add(description)
-        .add("}},\n");
-    buf = buf
-        .add("\tauthor={{")
-        .add(authors.join(" and ").as_str())
-        .add("}},\n");
-    let t = chrono::prelude::Local::now();
-    let month = t.month();
-    let year  = t.year();
-
-    let month_fmt = format!("\tmonth = {:0},\n", month);
-    let year_fmt  = format!("\tyear = {:0},\n", year);
-    buf = buf
-        .add(month_fmt.as_str())
-        .add(year_fmt.as_str());
-
-    buf = buf
-        .add("\turl = {{")
-        .add(url)
-        .add("}}\n");
-
-    buf.add("}\n")
 }
